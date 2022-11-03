c54271d77919156cdfc623550fa6e4542e5ea8eb
Eric Ng <EricNg109@gmail.com>
2022-11-03 14:30:54 -0700

    delete api key temp

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -6,7 +6,7 @@ class CivicsHttpClient: OkHttpClient() {
 
     companion object {
 
-        const val API_KEY = "AIzaSyAToD6tJgqSR9ocpITYwAeIRz7uzhr-HSc" //TODO: Place your API Key Here
+        const val API_KEY = "" //TODO: Place your API Key Here
 
         fun getClient(): OkHttpClient {
             return Builder()
